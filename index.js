55c490d1e5429aaba54fd57794fa7b616e83f3bb
Cédric <cedrictvg@gmail.com>
2023-05-11 02:23:26 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const http = require('http');
+const https = require('https');
+const url = require('url');
+const stringDecoder = require('string_decoder').StringDecoder;
+const config = require('./config');
+const fs = require('fs');
+
+// HTTP
+const httpServer = http.createServer((req, res) => {
+    unifiedServer(req, res);
+});
+
+httpServer.listen(config.httpPort, () => {
+    console.log('Server listening to port ' + config.httpPort);
+});
+
+
+// HTTPS
+const httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem')
+};
+
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+    unifiedServer(req, res);
+});
+
+httpsServer.listen(config.httpsPort, () => {
+    console.log('Server listening to port ' + config.httpsPort);
+});
+
+const unifiedServer = function (req, res) {
+    const parsedUrl = url.parse(req.url, true);
+
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    const queryStringObject = parsedUrl.query;
+
+    const method = req.method.toLowerCase();
+
+    const headers = req.headers;
+
+    const decoder = new stringDecoder('utf-8');
+    let buffer = '';
+
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        const data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
+
+        chosenHandler(data, (statusCode, payload) => {
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            const payloadString = JSON.stringify(payload);
+
+            res.setHeader('Content-Type', 'application/json');
+
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            console.log('Returning:', statusCode, payloadString);
+
+        });
+    });
+};
+
+// Routing
+const handlers = {};
+
+handlers.ping = function (data, callback) {
+    callback(200);
+};
+
+handlers.notFound = function (data, callback) {
+    callback(404);
+};
+
+const router = {
+    'ping': handlers.ping
+};
