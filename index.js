51de0dfb00adb2fa3dda577dbae6795939d58aaa
Cédric <cedrictvg@gmail.com>
2023-05-12 22:36:41 +0200

    Added background workers and log system

--- index.js
+++ index.js
@@ -1,90 +1,17 @@
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const stringDecoder = require('string_decoder').StringDecoder;
-const config = require('./lib/config');
-const fs = require('fs');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-// HTTP
-const httpServer = http.createServer((req, res) => {
-    unifiedServer(req, res);
+process.on('uncaughtException', function (err) {
+    console.log(err);
 });
 
-httpServer.listen(config.httpPort, () => {
-    console.log('Server listening to port ' + config.httpPort);
-});
+const app = {};
 
-
-// HTTPS
-const httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/cert.pem')
+app.init = function () {
+    server.init();
+    workers.init();
 };
 
-const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
-    unifiedServer(req, res);
-});
+app.init();
 
-httpsServer.listen(config.httpsPort, () => {
-    console.log('Server listening to port ' + config.httpsPort);
-});
-
-const unifiedServer = function (req, res) {
-    const parsedUrl = url.parse(req.url, true);
-
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-    const queryStringObject = parsedUrl.query;
-
-    const method = req.method.toLowerCase();
-
-    const headers = req.headers;
-
-    const decoder = new stringDecoder('utf-8');
-    let buffer = '';
-
-    req.on('data', (data) => {
-        buffer += decoder.write(data);
-    });
-
-    req.on('end', () => {
-        buffer += decoder.end();
-
-        const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-        const data = {
-            'trimmedPath': trimmedPath,
-            'queryStringObject': queryStringObject,
-            'method': method,
-            'headers': headers,
-            'payload': helpers.parseJsonToObject(buffer)
-        };
-
-        chosenHandler(data, (statusCode, payload) => {
-            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
-
-            payload = typeof (payload) == 'object' ? payload : {};
-
-            const payloadString = JSON.stringify(payload);
-
-            res.setHeader('Content-Type', 'application/json');
-
-            res.writeHead(statusCode);
-            res.end(payloadString);
-
-            console.log('Returning:', statusCode, payloadString);
-
-        });
-    });
-};
-
-// Routing
-const router = {
-    'ping': handlers.ping,
-    'users': handlers.users,
-    'tokens': handlers.tokens,
-    'checks': handlers.checks
-};
+module.exports = app;
